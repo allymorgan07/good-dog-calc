676aea97181223235a67ee0c21a71986b780e56c
allymorgan07 <60010177+allymorgan07@users.noreply.github.com>
2020-01-17 11:37:15 -0500

    Created Xcode Project

---
+++ Good Dog Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good Dog Calculator
+//
+//  Created by ESTEBAN, ALEXANDRA (LYHS) on 1/17/20.
+//  Copyright © 2020 Ally Esteban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# good-dog-calc
+# good dog calculator
 apcsp create task
