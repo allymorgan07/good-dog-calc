720b2bc81fb98ee3907b004455b8dbdf997c81f2
allymorgan07 <60010177+allymorgan07@users.noreply.github.com>
2020-01-17 11:55:08 -0500

    Test Button Wireframe

--- Good Dog Calculator/ViewController.swift
+++ Good Dog Calculator/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogView: UIImageView!
+    @IBOutlet weak var outputText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func uploadClicked(_ sender: Any) {
+        outputText.text = "good boy!"
+    }
+    
 
 }
 
